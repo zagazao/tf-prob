d873bdcb7821c4b52a0df692534d0ff8c08cb908
Matthias Jakobs <matthias.jakobs@tu-dortmund.de>
2020-10-02 14:42:30 +0200

    created model class and simplified training code

--- bnn.py
+++ bnn.py
@@ -1,373 +1,112 @@
-# Copyright 2018 The TensorFlow Probability Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ============================================================================
-"""Trains a Bayesian neural network to classify MNIST digits.
-
-The architecture is LeNet-5 [1].
-
-#### References
-
-[1]: Yann LeCun, Leon Bottou, Yoshua Bengio, and Patrick Haffner.
-     Gradient-based learning applied to document recognition.
-     _Proceedings of the IEEE_, 1998.
-     http://yann.lecun.com/exdb/publis/pdf/lecun-01a.pdf
-"""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import os
-import warnings
-
-import matplotlib
-# Dependency imports
-from absl import app
-from absl import flags
-from tensorflow_probability.python.layers import DenseVariational
-
-matplotlib.use('Agg')
-from matplotlib import figure  # pylint: disable=g-import-not-at-top
-from matplotlib.backends import backend_agg
 import numpy as np
-import tensorflow.compat.v2 as tf
+import tensorflow as tf
 import tensorflow_probability as tfp
-
-tf.enable_v2_behavior()
-
-# TODO(b/78137893): Integration tests currently fail with seaborn imports.
-warnings.simplefilter(action='ignore')
-
-try:
-    import seaborn as sns  # pylint: disable=g-import-not-at-top
-
-    HAS_SEABORN = True
-except ImportError:
-    HAS_SEABORN = False
+from abc import ABC, abstractmethod 
 
 tfd = tfp.distributions
 
-IMAGE_SHAPE = [28, 28, 1]
-NUM_TRAIN_EXAMPLES = 60000
-NUM_HELDOUT_EXAMPLES = 10000
-NUM_CLASSES = 10
+class ProbabilisticLayer(ABC):
 
-flags.DEFINE_float('learning_rate',
-                   default=0.001,
-                   help='Initial learning rate.')
-flags.DEFINE_integer('num_epochs',
-                     default=10,
-                     help='Number of training steps to run.')
-flags.DEFINE_integer('batch_size',
-                     default=128,
-                     help='Batch size.')
-flags.DEFINE_string('data_dir',
-                    default=os.path.join(os.getenv('TEST_TMPDIR', '/tmp'),
-                                         'bayesian_neural_network/data'),
-                    help='Directory where data is stored (if using real data).')
-flags.DEFINE_string(
-    'model_dir',
-    default=os.path.join(os.getenv('TEST_TMPDIR', '/tmp'),
-                         'bayesian_neural_network/'),
-    help="Directory to put the model's fit.")
-flags.DEFINE_integer('viz_steps',
-                     default=400,
-                     help='Frequency at which save visualizations.')
-flags.DEFINE_integer('num_monte_carlo',
-                     default=50,
-                     help='Network draws to compute predictive probabilities.')
-flags.DEFINE_bool('fake_data',
-                  default=False,
-                  help='If true, uses fake data. Defaults to real data.')
-
-FLAGS = flags.FLAGS
+    def __call__(self, x, inference=False):
+        pass 
 
 
-def plot_weight_posteriors(names, qm_vals, qs_vals, fname):
-    """Save a PNG plot with histograms of weight means and stddevs.
+class BayesianLinear(ProbabilisticLayer):
 
-    Args:
-      names: A Python `iterable` of `str` variable names.
-        qm_vals: A Python `iterable`, the same length as `names`,
-        whose elements are Numpy `array`s, of any shape, containing
-        posterior means of weight varibles.
-      qs_vals: A Python `iterable`, the same length as `names`,
-        whose elements are Numpy `array`s, of any shape, containing
-        posterior standard deviations of weight varibles.
-      fname: Python `str` filename to save the plot to.
-    """
-    fig = figure.Figure(figsize=(6, 3))
-    canvas = backend_agg.FigureCanvasAgg(fig)
+    def __init__(self, input_shape, hidden_units, sigma1=1.5, sigma2=0.1):
+        initializer_sigma = 0.6164414
 
-    ax = fig.add_subplot(1, 2, 1)
-    for n, qm in zip(names, qm_vals):
-        sns.distplot(tf.reshape(qm, shape=[-1]), ax=ax, label=n)
-    ax.set_title('weight means')
-    ax.set_xlim([-1.5, 1.5])
-    ax.legend()
+        self.hidden_shape = (input_shape, hidden_units)
+        self.hidden_units = hidden_units
 
-    ax = fig.add_subplot(1, 2, 2)
-    for n, qs in zip(names, qs_vals):
-        sns.distplot(tf.reshape(qs, shape=[-1]), ax=ax)
-    ax.set_title('weight stddevs')
-    ax.set_xlim([0, 1.])
+        # Define trainable parameter
+        self.kernel_mu = tf.Variable(tf.random.normal(shape=(input_shape, hidden_units), mean=0.0, stddev=initializer_sigma), name='kernel_mu')
+        self.kernel_rho = tf.Variable(tf.zeros(shape=(input_shape, hidden_units)), name='kernel_rho')
 
-    fig.tight_layout()
-    canvas.print_figure(fname, format='png')
-    print('saved {}'.format(fname))
+        self.bias_mu = tf.Variable(tf.random.normal(shape=[hidden_units], mean=0.0, stddev=initializer_sigma), name='bias_mu')
+        self.bias_rho = tf.Variable(tf.zeros(shape=[hidden_units]), name='bias_rho')
 
+        self.prior_bias = tfd.Independent(tfd.Mixture(
+            cat=tfd.Categorical(probs=np.tile([0.5, 0.5], (hidden_units, 1))),
+            components=[
+                tfd.Normal(loc=0.0, scale=tf.constant(sigma1, shape=(hidden_units))),
+                tfd.Normal(loc=0.0, scale=tf.constant(sigma2, shape=(hidden_units))),
+            ]), reinterpreted_batch_ndims=1)
 
-def plot_heldout_prediction(input_vals, probs,
-                            fname, n=10, title=''):
-    """Save a PNG plot visualizing posterior uncertainty on heldout data.
+        self.prior_kernel = tfd.Independent(tfd.Mixture(
+            cat=tfd.Categorical(probs=np.tile([0.5, 0.5], reps=(input_shape, hidden_units, 1))),
+            components=[
+                tfd.Normal(loc=0.0, scale=tf.constant(sigma1, shape=(input_shape, hidden_units))),
+                tfd.Normal(loc=0.0, scale=tf.constant(sigma2, shape=(input_shape, hidden_units))),
+            ]), reinterpreted_batch_ndims=2)
 
-    Args:
-      input_vals: A `float`-like Numpy `array` of shape
-        `[num_heldout] + IMAGE_SHAPE`, containing heldout input images.
-      probs: A `float`-like Numpy array of shape `[num_monte_carlo,
-        num_heldout, num_classes]` containing Monte Carlo samples of
-        class probabilities for each heldout sample.
-      fname: Python `str` filename to save the plot to.
-      n: Python `int` number of datapoints to vizualize.
-      title: Python `str` title for the plot.
-    """
-    fig = figure.Figure(figsize=(9, 3 * n))
-    canvas = backend_agg.FigureCanvasAgg(fig)
-    for i in range(n):
-        ax = fig.add_subplot(n, 3, 3 * i + 1)
-        ax.imshow(input_vals[i, :].reshape(IMAGE_SHAPE[:-1]), interpolation='None')
+        self.posterior_kernel = tfd.Independent(tfp.distributions.Normal(loc=self.kernel_mu, scale=tf.math.softplus(self.kernel_rho)), reinterpreted_batch_ndims=2)
+        self.posterior_bias = tfd.Independent(tfp.distributions.Normal(loc=self.bias_mu, scale=tf.math.softplus(self.bias_rho)), reinterpreted_batch_ndims=1)
 
-        ax = fig.add_subplot(n, 3, 3 * i + 2)
-        for prob_sample in probs:
-            sns.barplot(np.arange(10), prob_sample[i, :], alpha=0.1, ax=ax)
-            ax.set_ylim([0, 1])
-        ax.set_title('posterior samples')
+        self.log_prior = 0
+        self.log_posterior = 0
 
-        ax = fig.add_subplot(n, 3, 3 * i + 3)
-        sns.barplot(np.arange(10), tf.reduce_mean(probs[:, i, :], axis=0), ax=ax)
-        ax.set_ylim([0, 1])
-        ax.set_title('predictive probs')
-    fig.suptitle(title)
-    fig.tight_layout()
+    def get_trainable_vars(self):
+        return [self.kernel_mu, self.kernel_rho, self.bias_mu, self.bias_rho]
 
-    canvas.print_figure(fname, format='png')
-    print('saved {}'.format(fname))
+    def _reparametrize(self, mu, std, eps):
+        return mu + tf.math.softplus(std) * eps
+
+    def __call__(self, x, inference=False):
+        if inference:
+            return tf.matmul(x, self.kernel_mu) + self.bias_mu
+
+        W = self._reparametrize(self.kernel_mu, self.kernel_rho, tf.random.normal(shape=self.hidden_shape))
+        b = self._reparametrize(self.bias_mu, self.bias_rho, tf.random.normal(shape=[self.hidden_units]))
+
+        prior_log_prob_w = self.prior_kernel.log_prob(W)
+        posterior_log_prob_w = self.posterior_kernel.log_prob(W)
+
+        prior_log_prob_b = self.prior_bias.log_prob(b)
+        posterior_log_prob_b = self.posterior_bias.log_prob(b)
+
+        self.log_prior = prior_log_prob_w + prior_log_prob_b
+        self.log_posterior = posterior_log_prob_w + posterior_log_prob_b
+
+        return tf.matmul(x, W) + b
 
 
-DenseVariational
-# tf.keras.layers
+class BNN(object):
 
+    def __init__(self, layers):
+        self.layers = layers
 
-def create_model():
-    """Creates a Keras model using the LeNet-5 architecture.
+    def __call__(self, x, inference=False):
+        z = x
+        for layer in self.layers:
+            if isinstance(layer, ProbabilisticLayer):
+                z = layer(z, inference=inference)
+            else:
+                z = layer(z)
 
-    Returns:
-        model: Compiled Keras model.
-    """
-    # KL divergence weighted by the number of training samples, using
-    # lambda function to pass as input to the kernel_divergence_fn on
-    # flipout layers.
-    kl_divergence_function = (lambda q, p, _: tfd.kl_divergence(q, p) /  # pylint: disable=g-long-lambda
-                                              tf.cast(NUM_TRAIN_EXAMPLES, dtype=tf.float32))
+        return z
 
-    # Define a LeNet-5 model using three convolutional (with max pooling)
-    # and two fully connected dense layers. We use the Flipout
-    # Monte Carlo estimator for these layers, which enables lower variance
-    # stochastic gradients than naive reparameterization.
-    model = tf.keras.models.Sequential([
-        tfp.layers.Convolution2DFlipout(
-            6, kernel_size=5, padding='SAME',
-            kernel_divergence_fn=kl_divergence_function,
-            activation=tf.nn.relu),
-        tf.keras.layers.MaxPooling2D(
-            pool_size=[2, 2], strides=[2, 2],
-            padding='SAME'),
-        tfp.layers.Convolution2DFlipout(
-            16, kernel_size=5, padding='SAME',
-            kernel_divergence_fn=kl_divergence_function,
-            activation=tf.nn.relu),
-        tf.keras.layers.MaxPooling2D(
-            pool_size=[2, 2], strides=[2, 2],
-            padding='SAME'),
-        tfp.layers.Convolution2DFlipout(
-            120, kernel_size=5, padding='SAME',
-            kernel_divergence_fn=kl_divergence_function,
-            activation=tf.nn.relu),
-        tf.keras.layers.Flatten(),
-        tfp.layers.DenseFlipout(
-            84, kernel_divergence_fn=kl_divergence_function,
-            activation=tf.nn.relu),
-        tfp.layers.DenseFlipout(
-            NUM_CLASSES, kernel_divergence_fn=kl_divergence_function,
-            activation=tf.nn.softmax)
-    ])
+    def log_priors(self):
+        priors = 0.0
+        for layer in self.layers:
+            if isinstance(layer, ProbabilisticLayer):
+                priors += layer.log_prior
 
-    # Model compilation.
-    optimizer = tf.keras.optimizers.Adam(lr=FLAGS.learning_rate)
-    # We use the categorical_crossentropy loss since the MNIST dataset contains
-    # ten labels. The Keras API will then automatically add the
-    # Kullback-Leibler divergence (contained on the individual layers of
-    # the model), to the cross entropy loss, effectively
-    # calcuating the (negated) Evidence Lower Bound Loss (ELBO)
-    model.compile(optimizer, loss='categorical_crossentropy',
-                  metrics=['accuracy'], experimental_run_tf_function=False)
-    return model
+        return priors
 
+    def log_posteriors(self):
+        posteriors = 0.0
+        for layer in self.layers:
+            if isinstance(layer, ProbabilisticLayer):
+                posteriors += layer.log_posterior
 
-class MNISTSequence(tf.keras.utils.Sequence):
-    """Produces a sequence of MNIST digits with labels."""
+        return posteriors
 
-    def __init__(self, data=None, batch_size=128, fake_data_size=None):
-        """Initializes the sequence.
+    def get_trainable_vars(self):
+        trainable_vars = []
+        for layer in self.layers:
+            if isinstance(layer, ProbabilisticLayer):
+                trainable_vars += layer.get_trainable_vars()
 
-        Args:
-          data: Tuple of numpy `array` instances, the first representing images and
-                the second labels.
-          batch_size: Integer, number of elements in each training batch.
-          fake_data_size: Optional integer number of fake datapoints to generate.
-        """
-        if data:
-            images, labels = data
-        else:
-            images, labels = MNISTSequence.__generate_fake_data(
-                num_images=fake_data_size, num_classes=NUM_CLASSES)
-        self.images, self.labels = MNISTSequence.__preprocessing(
-            images, labels)
-        self.batch_size = batch_size
+        return trainable_vars
 
-    @staticmethod
-    def __generate_fake_data(num_images, num_classes):
-        """Generates fake data in the shape of the MNIST dataset for unittest.
-
-        Args:
-          num_images: Integer, the number of fake images to be generated.
-          num_classes: Integer, the number of classes to be generate.
-        Returns:
-          images: Numpy `array` representing the fake image data. The
-                  shape of the array will be (num_images, 28, 28).
-          labels: Numpy `array` of integers, where each entry will be
-                  assigned a unique integer.
-        """
-        images = np.random.randint(low=0, high=256,
-                                   size=(num_images, IMAGE_SHAPE[0],
-                                         IMAGE_SHAPE[1]))
-        labels = np.random.randint(low=0, high=num_classes,
-                                   size=num_images)
-        return images, labels
-
-    @staticmethod
-    def __preprocessing(images, labels):
-        """Preprocesses image and labels data.
-
-        Args:
-          images: Numpy `array` representing the image data.
-          labels: Numpy `array` representing the labels data (range 0-9).
-
-        Returns:
-          images: Numpy `array` representing the image data, normalized
-                  and expanded for convolutional network input.
-          labels: Numpy `array` representing the labels data (range 0-9),
-                  as one-hot (categorical) values.
-        """
-        images = 2 * (images / 255.) - 1.
-        images = images[..., tf.newaxis]
-
-        labels = tf.keras.utils.to_categorical(labels)
-        return images, labels
-
-    def __len__(self):
-        return int(tf.math.ceil(len(self.images) / self.batch_size))
-
-    def __getitem__(self, idx):
-        batch_x = self.images[idx * self.batch_size: (idx + 1) * self.batch_size]
-        batch_y = self.labels[idx * self.batch_size:(idx + 1) * self.batch_size]
-        return batch_x, batch_y
-
-
-def main(argv):
-    del argv  # unused
-    if tf.io.gfile.exists(FLAGS.model_dir):
-        tf.compat.v1.logging.warning(
-            'Warning: deleting old log directory at {}'.format(FLAGS.model_dir))
-        tf.io.gfile.rmtree(FLAGS.model_dir)
-    tf.io.gfile.makedirs(FLAGS.model_dir)
-
-    if FLAGS.fake_data:
-        train_seq = MNISTSequence(batch_size=FLAGS.batch_size,
-                                  fake_data_size=NUM_TRAIN_EXAMPLES)
-        heldout_seq = MNISTSequence(batch_size=FLAGS.batch_size,
-                                    fake_data_size=NUM_HELDOUT_EXAMPLES)
-    else:
-        train_set, heldout_set = tf.keras.datasets.mnist.load_data()
-        train_seq = MNISTSequence(data=train_set, batch_size=FLAGS.batch_size)
-        heldout_seq = MNISTSequence(data=heldout_set, batch_size=FLAGS.batch_size)
-
-    model = create_model()
-    # TODO(b/149259388): understand why Keras does not automatically build the
-    # model correctly.
-    model.build(input_shape=[None, 28, 28, 1])
-
-    print(' ... Training convolutional neural network')
-    for epoch in range(FLAGS.num_epochs):
-        epoch_accuracy, epoch_loss = [], []
-        for step, (batch_x, batch_y) in enumerate(train_seq):
-            batch_loss, batch_accuracy = model.train_on_batch(
-                batch_x, batch_y)
-            epoch_accuracy.append(batch_accuracy)
-            epoch_loss.append(batch_loss)
-
-            if step % 100 == 0:
-                print('Epoch: {}, Batch index: {}, '
-                      'Loss: {:.3f}, Accuracy: {:.3f}'.format(
-                    epoch, step,
-                    tf.reduce_mean(epoch_loss),
-                    tf.reduce_mean(epoch_accuracy)))
-
-            if (step + 1) % FLAGS.viz_steps == 0:
-                # Compute log prob of heldout set by averaging draws from the model:
-                # p(heldout | train) = int_model p(heldout|model) p(model|train)
-                #                   ~= 1/n * sum_{i=1}^n p(heldout | model_i)
-                # where model_i is a draw from the posterior p(model|train).
-                print(' ... Running monte carlo inference')
-                probs = tf.stack([model.predict(heldout_seq, verbose=1)
-                                  for _ in range(FLAGS.num_monte_carlo)], axis=0)
-                mean_probs = tf.reduce_mean(probs, axis=0)
-                heldout_log_prob = tf.reduce_mean(tf.math.log(mean_probs))
-                print(' ... Held-out nats: {:.3f}'.format(heldout_log_prob))
-
-                if HAS_SEABORN:
-                    names = [layer.name for layer in model.layers
-                             if 'flipout' in layer.name]
-                    qm_vals = [layer.kernel_posterior.mean()
-                               for layer in model.layers
-                               if 'flipout' in layer.name]
-                    qs_vals = [layer.kernel_posterior.stddev()
-                               for layer in model.layers
-                               if 'flipout' in layer.name]
-                    plot_weight_posteriors(names, qm_vals, qs_vals,
-                                           fname=os.path.join(
-                                               FLAGS.model_dir,
-                                               'epoch{}_step{:05d}_weights.png'.format(
-                                                   epoch, step)))
-                    plot_heldout_prediction(heldout_seq.images, probs,
-                                            fname=os.path.join(
-                                                FLAGS.model_dir,
-                                                'epoch{}_step{}_pred.png'.format(
-                                                    epoch, step)),
-                                            title='mean heldout logprob {:.2f}'
-                                            .format(heldout_log_prob))
-
-
-if __name__ == '__main__':
-    app.run(main)
--- layer_mnist.py
+++ layer_mnist.py
@@ -5,78 +5,9 @@ import tensorflow_probability as tfp
 from sklearn.datasets import fetch_openml
 from sklearn.preprocessing import StandardScaler
 
+from bnn import BayesianLinear, BNN
+
 tfd = tfp.distributions
-
-
-class BayesianLinear(object):
-
-    def __init__(self, input_shape, hidden_units, sigma1=1.5, sigma2=0.1):
-        initializer_sigma = 0.6164414
-
-        # Store shapes
-        self.hidden_shape = (input_shape, hidden_units)
-        self.hidden_units = hidden_units
-
-        # Define trainable parameter
-        self.kernel_mu = tf.Variable(tf.random.normal(shape=(input_shape, hidden_units), mean=0.0, stddev=initializer_sigma), name='kernel_mu')
-        self.kernel_rho = tf.Variable(tf.zeros(shape=(input_shape, hidden_units)), name='kernel_rho')
-
-        self.bias_mu = tf.Variable(tf.random.normal(shape=[hidden_units], mean=0.0, stddev=initializer_sigma), name='bias_mu')
-        self.bias_rho = tf.Variable(tf.zeros(shape=[hidden_units]), name='bias_rho')
-
-        # Specify prior as independent normal distributions..
-        self.prior_bias = tfd.Independent(tfd.Mixture(
-            cat=tfd.Categorical(probs=np.tile([0.5, 0.5], (hidden_units, 1))),
-            components=[
-                tfd.Normal(loc=0.0, scale=tf.constant(sigma1, shape=(hidden_units))),
-                tfd.Normal(loc=0.0, scale=tf.constant(sigma2, shape=(hidden_units))),
-            ]), reinterpreted_batch_ndims=1)
-
-        self.prior_kernel = tfd.Independent(tfd.Mixture(
-            cat=tfd.Categorical(probs=np.tile([0.5, 0.5], reps=(input_shape, hidden_units, 1))),
-            components=[
-                tfd.Normal(loc=0.0, scale=tf.constant(sigma1, shape=(input_shape, hidden_units))),
-                tfd.Normal(loc=0.0, scale=tf.constant(sigma2, shape=(input_shape, hidden_units))),
-            ]), reinterpreted_batch_ndims=2)
-
-        # Specify prior as independent normal distributions given estimated neural net parameters..
-        self.posterior_kernel = tfd.Independent(tfp.distributions.Normal(loc=self.kernel_mu, scale=tf.math.softplus(self.kernel_rho)), reinterpreted_batch_ndims=2)
-        self.posterior_bias = tfd.Independent(tfp.distributions.Normal(loc=self.bias_mu, scale=tf.math.softplus(self.bias_rho)), reinterpreted_batch_ndims=1)
-
-        # Specify noise distribution...
-        # self.dist_eps = tfp.distributions.Normal(loc=0.0, scale=0.1)
-
-        self.kl_div = 0
-
-        self.log_prior = 0
-        self.log_posterior = 0
-
-    def get_trainable_vars(self):
-        return [self.kernel_mu, self.kernel_rho, self.bias_mu, self.bias_rho]
-
-    def _reparametrize(self, mu, std, eps):
-        return mu + tf.math.softplus(std) * eps
-
-    def forward(self, x, inference=False):
-        # Sample W and bias...
-
-        if inference:
-            return tf.matmul(x, self.kernel_mu) + self.bias_mu
-
-        W = self._reparametrize(self.kernel_mu, self.kernel_rho, tf.random.normal(shape=self.hidden_shape))
-        b = self._reparametrize(self.bias_mu, self.bias_rho, tf.random.normal(shape=[self.hidden_units]))
-
-        prior_log_prob_w = self.prior_kernel.log_prob(W)
-        posterior_log_prob_w = self.posterior_kernel.log_prob(W)
-
-        prior_log_prob_b = self.prior_bias.log_prob(b)
-        posterior_log_prob_b = self.posterior_bias.log_prob(b)
-
-        self.log_prior = prior_log_prob_w + prior_log_prob_b
-        self.log_posterior = posterior_log_prob_w + posterior_log_prob_b
-
-        return tf.matmul(x, W) + b
-
 
 x, y = fetch_openml('mnist_784', version=1, return_X_y=True)
 
@@ -101,8 +32,11 @@ test_data = test_data.batch(batch_size)
 n_batches = len(train_data)
 n_testbatches = len(test_data)
 
-l1 = BayesianLinear(784, 1000)
-l2 = BayesianLinear(1000, 10)
+model = BNN([
+            BayesianLinear(784, 1000), 
+            tf.nn.relu, 
+            BayesianLinear(1000, 10)
+        ])
 
 opt = tf.optimizers.Adam(learning_rate=0.08)
 
@@ -115,21 +49,21 @@ for epochs in range(10):
     for batch_id, (x, y) in enumerate(train_data):
         with tf.GradientTape() as tape:
 
-            outputs, log_priors, log_posteriors = [], [], []
+            outputs, log_priors, log_posteriors = [], 0.0, 0.0
 
             for wi in range(W_SAMPLES):
-                z = l1.forward(x)
-                z = tf.nn.relu(z)
-                z = l2.forward(z)
+                z = model(x)
 
                 outputs.append(z)
-                log_priors.append(l1.log_prior + l2.log_prior)
-                log_posteriors.append(l1.log_posterior + l2.log_posterior)
+                log_priors += model.log_priors()
+                log_posteriors += model.log_posteriors()
 
             outputs = tf.convert_to_tensor(outputs)  # Shape w_samples, batch_size, 2
             log_priors = tf.convert_to_tensor(log_priors)  # shape w_samples
             log_posteriors = tf.convert_to_tensor(log_posteriors)  # shape w_samples
 
+            #log_priors = tf.reduce_sum(log_priors)
+            #log_posteriors = tf.reduce_sum(log_posteriors)
             # means = outputs
             # means = tf.reduce_mean(outputs, axis=0)
             # stddevs = tf.math.softplus(outputs[..., 1])
@@ -141,22 +75,20 @@ for epochs in range(10):
             # kl_weights = tf.cast((tf.pow(2, n_batches - batch_id)) / (tf.pow(2, n_batches) - 1), tf.float32)
             kl_weights = 1 / n_batches
 
-            loss = kl_weights * (tf.reduce_sum(log_posteriors) - tf.reduce_sum(log_priors)) - tf.reduce_sum(log_likelihood) * 49000 / 128
+            loss = kl_weights * (log_posteriors - log_priors) - tf.reduce_sum(log_likelihood) * 49000 / 128
             running_loss += loss
             # print('{:10.3f} - {:10.3f} - {:10.3f}'.format(log_likelihood.numpy().mean(), loss.numpy(), mse.numpy()))
 
         # print(np.mean(mses))
         # df/dw, muss man da nicht das gesampelte (gemittelte) w nehmen? also tape.gradient(loss, w_samples) ? 
-        trainable_vars = l1.get_trainable_vars() + l2.get_trainable_vars()
+        trainable_vars = model.get_trainable_vars()
 
         grads = tape.gradient(loss, trainable_vars)
         opt.apply_gradients(zip(grads, trainable_vars))
 
     correct_test = 0.0
     for batch_id, (x, y) in enumerate(test_data):
-        z = l1.forward(x, inference=True)
-        z = tf.nn.relu(z)
-        z = l2.forward(z, inference=True)
+        z = model(x, inference=True)
         preds = tf.nn.softmax(z)
 
         binary_comp = tf.cast(tf.argmax(preds, axis=1), tf.float32) == y
